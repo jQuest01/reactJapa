2c847f0007932c41c5ae6db9489fd3a7cf00fa2b
jQuest01 <guicamposf16@hotmail.com>
2020-02-14 19:33:50 -0300

    Salve quebrada

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./components/Header"
+import './styles.css'
+import Main from './pages/main/index'
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
